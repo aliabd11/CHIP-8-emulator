cfc8006a9c666fd0b515ab7bfc4e3c6291aafd14
Abdullah <abd.ali@mail.utoronto.ca>
2017-05-28 17:59:15 -0400

    to finish later

---
+++ cpu.c
@@ -0,0 +1,4 @@
+//will move main cpu functions here later for modularity sake
+//main opcode implementation to be moved here as well
+
+void emulate_cycle(Chip8System *chip8, Main_Display *display);
